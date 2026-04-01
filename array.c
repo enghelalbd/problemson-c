539b206aa4e95bc6ec057e4d667f487ec6e4dadf
enghelalbd <informhelal@yahoo.com>
2026-04-01 11:02:47 +0600

    one diamosonal arr sum added

--- array.c
+++ array.c
@@ -1,18 +1,31 @@
-
-
 #include<stdio.h>
-
 int main(){
-int sum=0, ar[5],i;
-printf(" Enter The value");
-for(i=0;i<10;i++)
-    {
-scanf("%d",&ar[i]);
-}
- for(i=0;i<=10;i++)
- {
-     sum=sum+ar[i];
+    int sum=0, sumood=0,sumeven=0,i,n;
+    printf("Enter The Array Length:");
+    scanf("%d",&n);
+    int arr[n];
 
- }
- printf("Sum is:%d",sum);
+    printf("Enter The value of Array:\n");
+    for(i=0;i<n;i++){
+         scanf("%d",&arr[i]);
+
+         sum=sum+arr[i];
+         if (arr[i]%2==0){
+            sumood=sumood+arr[i];
+         }
+         else{
+            sumeven=arr[i]+sumeven;
+         }
+
+    }
+    printf("Sum of Array is:%d\n",sum);
+    printf("Sum of Evenvalue is:%d\n",sumood);
+    printf("Sum of Oddvalue is:%d\n",sumeven);
+
+
+
+
+
+
+
 }
