f280e78d95396543fca1882aca55efa7af7752c1
enghelalbd <informhelal@yahoo.com>
2026-04-17 16:21:51 +0600

    Trangle Area Added

---
+++ areatrangle.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+  int b,h;
+  float area=0;
+  printf(" Enter The value of  Base and hight :");
+  scanf("%d %d",&b,&h);
+  area =(b*h)/2.0;
+  printf(" Area of Trangle is :%.2f",area);
+  
+}
---
+++ tranglearea.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+  int b,h;
+  float area=0;
+  printf(" Enter The value of  Base and hight :");
+  scanf("%d %d",&b,&h);
+  area =(float)(b*h)/2;
+  printf(" Area of Trangle is :%.2f",area);
+  
+}
