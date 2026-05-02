c90c74ec015a3286a63a0ae9d47867439c9d247d
enghelalbd <informhelal@yahoo.com>
2026-05-02 16:26:31 +0600

    divisible by 5

---
+++ divisibleby5.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+
+{
+    int n;
+   printf(" Enter The value:\n") ;
+   scanf("%d",&n);
+   if(n%5==0){
+    printf("The Number Is visible by 5 \n ");
+   }
+else
+{
+    printf("This not divisible by 5 \n");
+}
+
+
+}
