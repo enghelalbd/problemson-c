ac9b61549268d2f0d31cc9371b953d47edb1eb6a
enghelalbd <informhelal@yahoo.com>
2026-05-02 16:37:47 +0600

    Positive nagative

---
+++ positiveornagative.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main()
+
+{
+    int n;
+   printf(" Enter The value:\n") ;
+   scanf("%d",&n);
+
+   if(n<0)
+   {
+       printf(" The value is Negative:\n");
+
+   }
+   else if (n>0){
+    printf(" THe value is Positive");
+   }
+   else
+    printf("The  Value  is Zroe: ");
+
+
+
+}
